6b9474d777050a7f02c2659417324ad250d8f30d
Gaurav <shahgauravraj@gmail.com>
2020-09-11 20:09:55 +0545

    added this for separate widget to all transactions

---
+++ lib/widgets/user_transactions.dart
@@ -0,0 +1,55 @@
+import 'package:ExpenseMonitor/widgets/new_transaction.dart';
+import 'package:ExpenseMonitor/widgets/transactions_list.dart';
+import 'package:ExpenseMonitor/models/transaction.dart';
+import 'package:flutter/material.dart';
+
+class UserTransactions extends StatefulWidget {
+  @override
+  _UserTransactionsState createState() => _UserTransactionsState();
+}
+
+class _UserTransactionsState extends State<UserTransactions> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 59.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Grocery',
+      amount: 10.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Stationery',
+      amount: 11.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        NewTransaction(_addNewTransaction),
+        TransactionList(_userTransactions),
+      ],
+    );
+  }
+}
